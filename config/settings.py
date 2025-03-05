d16b922a55c71cdf40661038ece817a5472c6157
A1909bek <aa2004bek@gmail.com>
2025-03-05 15:24:51 +0500

    next commit

--- config/settings.py
+++ config/settings.py
@@ -32,8 +32,14 @@ INSTALLED_APPS = [
 
     #external
     'rest_framework',
-    'rest_frmework_simplejwt',
+    'rest_framework_simplejwt',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
