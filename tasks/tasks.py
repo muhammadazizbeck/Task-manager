94005a6141f79476199475806ca30c317cdd31ce
A1909bek <aa2004bek@gmail.com>
2025-03-08 14:01:21 +0500

    next commit

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -3,4 +3,4 @@ from celery import shared_task
 @shared_task
 def send_notification(user_id,message):
     print(f"User {user_id} ga xabar yuborildi {message}")
-    
+    return f"Xabar yuborildi {message}"
